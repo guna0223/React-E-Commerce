401559919849de1869f7f3777384167472f86909
guna0223 <gunasekar0223@gmail.com>
2025-10-15 11:21:50 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Outlet, Link } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../components/Carousel/Carousel";
+
+const Home = () => {
+    return (
+        <>
+            <Carousel />
+        </>
+
+    )
+}
+
+export default Home;
